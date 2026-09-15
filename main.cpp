8572c80931fdf76319fab628274126227cbf5286
e-manohall2 <eamonn.ohalloran@icloud.com>
2026-09-15 11:01:47 -0500

    Made the make file, wrote the code for assignment 2, changed VS code

--- main.cpp
+++ main.cpp
@@ -39,11 +39,56 @@ int main( int argc, char * argv[] )
 			i++;
 		}
 	}
+    cout.setf(ios::fixed);
+	cout.setf(ios::showpoint);
+	cout.precision(2);
 
+	double intrest_sum=0;
+	int month=0;
 	loan_amount = arguments[0];
 	yearly_interest_rate = arguments[1];
 	monthly_payment = arguments[2];
-	cout << loan_amount << " " << yearly_interest_rate << " " << monthly_payment << endl;
+	if(monthly_payment<(yearly_interest_rate/1200*loan_amount)){ //if intrest is greater than payment 
+		cout << "(Insufficient payment): " << argv[i-2] << " " << argv[i-1] << " " << argv[i] << endl;
+
+	}
+
+
+	double instantaneous_intrest;
+	cout<<"******************************************************"<<endl;
+	cout<<"\tAmortized Table"<<endl;
+	cout<<"******************************************************"<<endl;
+
+	cout<<"Month\tBalance\tPayment\tRate\tIntrest\tPrinciple"<<endl;
+
+	while(loan_amount>0.00){
+		if(month==0){
+			cout<<month<<"\t"<<loan_amount<<"\tN/A\tN/A\tN/A\tN/A"<<endl;
+		}
+		else{
+			instantaneous_intrest = loan_amount * yearly_interest_rate /1200;
+			if(monthly_payment<(loan_amount+instantaneous_intrest)){
+				loan_amount+=instantaneous_intrest;
+				loan_amount-=monthly_payment;
+				intrest_sum += instantaneous_intrest;
+				cout<<month<<"\t$"<<loan_amount<<"\t$"<<monthly_payment<<"\t"<<yearly_interest_rate/(12.0)<<"\t$"<<instantaneous_intrest<<"\t$"<<monthly_payment-instantaneous_intrest<<endl;
+			}
+			else{
+
+				loan_amount+=instantaneous_intrest;
+				cout<<month<<"\t$"<<0.00<<"\t$"<<loan_amount<<"\t"<<yearly_interest_rate/(12.0)<<"\t$"<<instantaneous_intrest<<"\t$"<<loan_amount-instantaneous_intrest<<endl;
+				loan_amount-=monthly_payment;
+				loan_amount = (loan_amount<0)?0:loan_amount;
+			}
+
+		}
+		month++;
+	}
+	cout<<"******************************************************\n"<<endl;
+	cout<<"It takes "<<month<<" to pay off the loan."<<endl;
+	cout<<"Total intrest paid is: $"<<intrest_sum;
+
+
 
 	return 0;
 }
--- makefile
+++ makefile
@@ -1,15 +1,13 @@
 #modify this makefile so that it will work for this new assignment
 CC=g++
-DEPS = starter.h
+V =-std=c++11
 
-all: starter.o main.o
-	$(CC) -std=c++11 starter.o main.o
+all:  main.o
+	$(CC) $(V) main.o -o a
 
-bowling: starter.o $(DEPS)
-	$(CC) -c -std=c++11 starter.cpp
-
-main: main.o $(DEPS)
-	$(CC) -c -std=c++11 main.cpp
+main: main.o 
+	$(CC) -c $(V) main.cpp
 
 clean: 
-	rm *.o *.out
+	rm *.o 
+	rm *.out
